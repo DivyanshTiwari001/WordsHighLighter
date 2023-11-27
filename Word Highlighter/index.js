816d2ceca7b1caed801dba7d5a7202bc7811b235
Divyansh <divyanshtiwari4237@gmail.com>
2023-11-27 15:37:46 +0530

    made first improvement

--- Word Highlighter/index.js
+++ Word Highlighter/index.js
@@ -1,27 +1,39 @@
-const elem = document.getElementsByTagName('body');
-const search = prompt();
-const words = search.split(',');
-console.log(words);
-const prevContent = elem[0].innerHTML;
-let str = elem[0].innerHTML;
-elem[0].innerHTML = "";
-words.forEach(word=>{
-    const arr = []
-    let regex =new RegExp(word,'gi');
-    while((result = regex.exec(str))){
-        arr.push(result.index);
-        console.log(result.index);
+const elem = document.getElementsByTagName('body')[0];
+const prevContent = elem.innerHTML;
+elem.addEventListener('keydown',()=>{
+    let code = event.key;
+    if(code=='S' || code=='s'){
+        elem.innerHTML = prevContent;
+        searchWords();
     }
-    let ans = ""
-    let j = 0;
-    let r = Math.floor(Math.random()*256)
-    let g = Math.floor(Math.random()*256)
-    let b = Math.floor(Math.random()*256)
-    arr.forEach(i=>{
-        ans += str.slice(j,i)+`<span style="background-color:rgb(${r},${g},${b})">`+str.slice(i,i+word.length)+'</span>'
-        j = i+word.length;
-    });
-    ans+=str.slice(j,str.length);
-    str = ans;
 })
-elem[0].innerHTML = str;
+const searchWords = () => {
+    const search = prompt();
+    if(search.length==0){
+        return;
+    }
+    const words = search.split(',');
+    let str = elem.innerHTML;
+    elem.innerHTML = "";
+    words.forEach(word => {
+        const arr = []
+        let regex = new RegExp(word, 'gi');
+        let result;
+        while ((result = regex.exec(str))) {
+            arr.push(result.index);
+        }
+        if(arr.length==0)return;
+        let ans = ""
+        let j = 0;
+        let r = Math.floor(Math.random() * 256)
+        let g = Math.floor(Math.random() * 256)
+        let b = Math.floor(Math.random() * 256)
+        arr.forEach(i => {
+            ans += str.slice(j, i) + `<span style="background-color:rgb(${r},${g},${b})">` + str.slice(i, i + word.length) + '</span>'
+            j = i + word.length;
+        });
+        ans += str.slice(j, str.length);
+        str = ans;
+    })
+    elem.innerHTML = str;
+}
